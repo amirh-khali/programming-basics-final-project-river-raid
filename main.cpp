3cb31a5ef3014de719f4ba431a0be53b4908f8e9
AmirH Ahmadi <amirh.khali@gmail.com>
2019-01-09 14:10:40 +0330

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ever (;;)
+
+typedef long long ll;
+
+const int ScreenLength = 480;
+cinst int ScreenWidth = 480;
+
+int main () {
+
+    return 0;
+}
